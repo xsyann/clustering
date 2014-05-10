54910d564d35db0a8dc279aa9330c720baa6d919
xsyann <ykoeth@xsyann.com>
2014-05-10 17:57:19 +0200

    Dynamic features

--- clusterer.py
+++ clusterer.py
@@ -6,31 +6,89 @@
 ## Contact <contact@xsyann.com>
 ##
 ## Started on  Fri Apr 25 18:16:06 2014 xs_yann
-## Last update Thu May  8 22:40:11 2014 xs_yann
+## Last update Sat May 10 17:51:33 2014 xs_yann
 ##
 
 import os
 import time
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class Clusterer:
     """
     Extract image clusters.
     """
 
-    RANDOM = 0
-    PLUSPLUS = 1
-    K = 2 # TODO: Dynamic K
+    KMEANS = 1
+    KMEANS_PP = 2
+    THRESHOLD = 3
 
-    __modes = { RANDOM: cv2.KMEANS_RANDOM_CENTERS, PLUSPLUS: cv2.KMEANS_PP_CENTERS }
+    FEATURE_R = 1 << 0
+    FEATURE_G = 1 << 1
+    FEATURE_B = 1 << 2
+    FEATURE_H = 1 << 3
+    FEATURE_S = 1 << 4
+    FEATURE_V = 1 << 5
 
-    def getClusters(self, imagePath, backgroundColor=(0, 0, 0), mode=PLUSPLUS, verbose=True):
+    FEATURE_RGB = FEATURE_R | FEATURE_G | FEATURE_B
+
+    GRAPH_R = 1
+    GRAPH_G = 2
+    GRAPH_B = 3
+    GRAPH_H = 4
+    GRAPH_S = 5
+    GRAPH_V = 6
+
+    GRAPH_RGB = [GRAPH_R, GRAPH_G, GRAPH_B]
+
+
+    K = 3 # Cluster Count
+
+    __modes = { KMEANS: cv2.KMEANS_RANDOM_CENTERS,
+                KMEANS_PP: cv2.KMEANS_PP_CENTERS }
+
+    # Private Static methods
+
+    def __computeRGB(image):
+        return np.float32(image.reshape((-1, 3)))
+
+    def __hashRGB(flag, samples):
+        table = { Clusterer.FEATURE_R: samples[:,2:3],
+                  Clusterer.FEATURE_G: samples[:,1:2],
+                  Clusterer.FEATURE_B: samples[:,0:1] }
+        return table[flag]
+
+    def __computeHSV(image):
+        return np.float32(cv2.cvtColor(image, cv2.COLOR_BGR2HSV).reshape((-1, 3)))
+
+    def __hashHSV(flag, samples):
+        table = { Clusterer.FEATURE_H: samples[:,0:1],
+                  Clusterer.FEATURE_S: samples[:,1:2],
+                  Clusterer.FEATURE_V: samples[:,2:3] }
+        return table[flag]
+
+    # Features
+
+    __featuresTable = [((FEATURE_R, FEATURE_G, FEATURE_B), __computeRGB, __hashRGB),
+                       ((FEATURE_H, FEATURE_S, FEATURE_V), __computeHSV, __hashHSV) ]
+
+    # Public methods
+
+    def getClusters(self, imagePath, mode=KMEANS_PP, features=FEATURE_RGB,
+                    graph=GRAPH_RGB, centers=True, data=True,
+                    backgroundColor=(0, 0, 0), verbose=True):
         """Returns clusters in image.
         """
         img = self.__readImage(imagePath)
-        retval, labels, centers = self.__kmeans(img, mode, verbose)
+        if mode == self.THRESHOLD:
+            labels = self.__threshold(img, verbose)
+        else:
+            retval, labels, centers = self.__kmeans(img, mode, features, verbose)
         return self.__getClustersImages(img, labels, backgroundColor, verbose)
+
+    # Private methods
 
     def __getClustersImages(self, image, labels, backgroundColor, verbose):
         """Create clusters images from k-means results.
@@ -53,17 +111,60 @@ class Clusterer:
 
         return cluster.reshape(image.shape)
 
-    def __kmeans(self, image, mode, verbose):
+    def __threshold(self, image, verbose):
+        if verbose:
+            self.__startTime = time.time()
+            print "Threshold computing..."
+        flatten = np.copy(np.copy(image).reshape((-1, 3)))
+#        gray = np.uint8(np.sum(flatten, axis=1) / 3)
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV).reshape((-1, 3))
+        hsv = hsv[:,0]
+#        range = 255 / self.K
+        range = 180 / self.K
+        labels = np.zeros_like(hsv)
+        for i in xrange(1, self.K):
+            labels += (hsv > (range * i))
+            #labels += (gray > (range * i))
+
+        return labels
+
+    def __getFeatures(self, image, flags):
+        """Extract features from features flags and stack them in an array.
+        """
+        samples = []
+        for components, compute, hash in self.__featuresTable:
+            if self.__hasComponent(flags, components):
+                samples.append((components, compute(image), hash))
+
+        features = None
+        for i in xrange(flags.bit_length()):
+            flag = ((flags >> i) & 1) << i
+            if flag:
+                for components, sample, hash in samples:
+                    if flag in components:
+                        features = hash(flag, sample) if features is None else \
+                                 np.hstack((features, hash(flag, sample)))
+        return features
+
+    def __hasComponent(self, flags, components):
+        """Return True if at least one item in components is in flags.
+        """
+        return sum([flags & f for f in components]) > 0
+
+    def __kmeans(self, image, mode, features, verbose):
         """Apply K-Means algorithm on image.
         """
-        samples = np.float32(image.reshape((-1, 3)))
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-
         if verbose:
             self.__startTime = time.time()
             print "K-Means computing..."
 
-        return cv2.kmeans(samples, self.K, criteria, 10, self.__modes[mode])
+        samples = self.__getFeatures(image, features)
+
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        retval, labels, centers = cv2.kmeans(samples, self.K,
+                                             criteria, 10, self.__modes[mode])
+
+        return retval, labels, centers
 
     def __readImage(self, path):
         """Load image from path.
--- clustering.py
+++ clustering.py
@@ -6,7 +6,7 @@
 ## Contact <contact@xsyann.com>
 ##
 ## Started on  Fri Apr 25 18:16:06 2014 xs_yann
-## Last update Thu May  8 22:41:41 2014 xs_yann
+## Last update Sat May 10 17:52:03 2014 xs_yann
 ##
 
 import os
@@ -18,6 +18,27 @@ from matplotlib import pyplot as plt
 
 from clusterer import Clusterer
 
+class ClustererArgsParser:
+
+    FEATURES_TABLE = { 'r': Clusterer.FEATURE_R,
+                       'g': Clusterer.FEATURE_G,
+                       'b': Clusterer.FEATURE_B,
+                       'h': Clusterer.FEATURE_H,
+                       's': Clusterer.FEATURE_S,
+                       'v': Clusterer.FEATURE_V }
+
+    def __init__(self, features):
+        self.__features = features
+
+    @property
+    def features(self):
+        """Convert "rgb" into FLAG_R | FLAG_G | FLAG_B.
+        """
+        flags = 0
+        for f in self.__features:
+            flags |= self.FEATURES_TABLE[f]
+        return flags
+
 class Clustering:
 
     ESC = 27
@@ -25,12 +46,23 @@ class Clustering:
     KEY_RIGHT = 3
     PREVIEW_HEIGHT = 850
 
-    def __init__(self, images):
+    def __init__(self, images, features):
         self.__index = 0
         clusterer = Clusterer()
         self.clusters = {}
+
+        argsParser = ClustererArgsParser(features)
+
+        features = argsParser.features
+
+        graph = [Clusterer.GRAPH_R, Clusterer.GRAPH_G]
         for image in images:
-            self.clusters[image] = clusterer.getClusters(image)
+            self.clusters[image] = clusterer.getClusters(image,
+                                                         mode=Clusterer.KMEANS_PP,
+                                                         features=features,
+                                                         graph=graph,
+                                                         centers=True,
+                                                         data=True)
 
     def __showClusters(self, index):
         for image, clusters in self.clusters.iteritems():
@@ -60,15 +92,31 @@ class Clustering:
                 self.__showClusters(self.__index)
         cv2.destroyAllWindows()
 
+def graph_arg(x):
+    allowed = ClustererArgsParser.FEATURES_TABLE
+    if not (1 <= len(x) <= 3) or sum([c in allowed for c in x]) != len(x):
+        raise argparse.ArgumentTypeError("%s is not a graph arg" % x)
+    return x
+
+def features_arg(x):
+    allowed = ClustererArgsParser.FEATURES_TABLE
+    if sum([c in allowed for c in x]) != len(x):
+        raise argparse.ArgumentTypeError("%s is not a features arg" % x)
+    return x
+
 if __name__ == "__main__":
     import argparse, sys
 
     parser = argparse.ArgumentParser(description="Clustering")
     parser.add_argument("image", type=str, nargs="+", help="Input image")
+    parser.add_argument("-f", "--features", type=features_arg, default='rgb')
+    parser.add_argument("-d", "--graph-data", type=graph_arg)
+    parser.add_argument("-a", "--graph-all", type=graph_arg)
+    parser.add_argument("-c", "--graph-centers", type=graph_arg)
     args = parser.parse_args()
 
     try:
-        clustering = Clustering(args.image)
+        clustering = Clustering(args.image, args.features)
         clustering.run()
     except (OSError, cv2.error) as err:
         print err
